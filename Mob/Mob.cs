8c7d71cc1fb93b644e257a0f92aa78d3b0e82351
Jacob Johnkoski <jacobjohnkoski@gmail.com>
2021-10-31 22:51:17 -0500

    Added Main scene with timers and starting position of player

---
+++ Mob/Mob.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Mob : RigidBody2D
+{
+    [Export]
+    public int MinMovementSpeed = 150; 
+    [Export]
+    public int MaxMovementSpeed = 250;
+
+    static private Random _random = new Random();
+
+    public override void _Ready()
+    {
+        var sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        var mobTypes = sprite.Frames.GetAnimationNames();
+        sprite.Animation = mobTypes[_random.Next(0, mobTypes.Length)];
+    }
+
+    public void OnVisibilityNotifier2DScreenExited()
+    {
+        QueueFree();
+    }
+
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+}
